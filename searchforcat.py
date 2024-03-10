4d5ef184f1cd01b8335946d12c818a43833dc436
gamestartergame <113556662+gamestartergame@users.noreply.github.com>
2024-03-10 19:47:56 +0000

    more fixes

--- searchforcat.py
+++ searchforcat.py
@@ -2,6 +2,7 @@ import os
 import time
 import urllib.error
 import googlesearch
+import subprocess
 os.system("title cat search (made by gamestartergame)")
 print("github: https://github.com/gamestartergame")
 print("youtube: https://www.youtube.com/channel/UCk7nqcjq4od5hbf039Vn2QQ")
@@ -11,11 +12,14 @@ f = open(website + " with cats.txt", "w")
 f.write("if theres nothing in this file and you searched it means you hit a error\n")
 numofresults =input("how many results do you want? \n")
 input("press enter to start searching for cats\n")
+print("opening console for debug")
+cmd = subprocess.Popen("cmd.exe /K cd c:/")
 import googlesearch
 from googlesearch import search
 
 
 def google_search(count):
+
  time.sleep(1)
  print("searching.. might take a while (because of error 429 :/)")
  print( "debug: "+ numofresults)
@@ -25,7 +29,11 @@ def google_search(count):
   f.write("found results with "+ cat +" result number: "+ str(count) +"\n")
   f.write(res + "\n")
   print("found: " + res +" "+ str(count) +"\n")
+ cmd.terminate()
  time.sleep(1)
+
+
+
 
 if int(numofresults) <= 200:
 
@@ -34,4 +42,3 @@ if int(numofresults) <= 200:
 else:
  print("can not find results higher than 200")
 time.sleep(1)
-exit()
