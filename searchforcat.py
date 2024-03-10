17a009c29f9f95f0312440653b0190dc96596cff
gamestartergame <113556662+gamestartergame@users.noreply.github.com>
2024-03-10 03:35:10 +0000

    more changes
    ugh 429 status error annoying me

--- searchforcat.py
+++ searchforcat.py
@@ -6,7 +6,7 @@ f =open("youtube with cats.txt" , "w")
 input("press enter to start searching for cats\n")
 print("searching.. might take a while (if crashes ever is a error or too many requests)")
 from googlesearch import search
-for results in search("cats site:youtube.com" , tld="co.in" , num=500, stop=500 , pause=5):
+for results in search("cat site:youtube.com" , tld="co.in" , num=100, stop=100 , pause=20):
  f.write(results + "\n")
  print("cat search done")
  y_n =input("do you want to open one? y or n\n")
