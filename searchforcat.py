b1854bf12ccf7509836ce9d2a78a6160cf5cdfaf
gamestartergame <113556662+gamestartergame@users.noreply.github.com>
2024-03-10 17:46:02 +0000

    update!

--- searchforcat.py
+++ searchforcat.py
@@ -6,20 +6,27 @@ os.system("title cat search")
 website =input("what website do you want to search cats on\n")
 cat = input("cats:\n")
 f = open("youtube with cats.txt", "w")
-f.write("if theres nothing in this file and you searched it means you hit a error")
+f.write("if theres nothing in this file and you searched it means you hit a error\n")
+numofresults =input("how many results do you want? \n")
 input("press enter to start searching for cats\n")
-print("searching.. might take a while (because of error 429 :/)")
 import googlesearch
 from googlesearch import search
 
 
 def google_search(t):
     time.sleep(1)
-    for results in search("cats" + cat + " site:" + website, tld="co.in", num=10, stop=10, pause=5):
+    print("searching.. might take a while (because of error 429 :/)")
+    for results in search("cats" + cat + " site:" + website, tld="co.in", num=int(numofresults), stop=10, pause=5):
+        f.write("found results with " +cat +"\n")
         f.write(results + "\n")
         print("found: " + results + "\n")
 time.sleep(1)
 
+if int(numofresults) <= 100:
 
+ google_search(t=0)
 
-google_search(t=0)
+else:
+ print("can not find results higher than 100")
+time.sleep(1)
+exit()
